c6310b3c81b5e767bd3e2b5b573b6d71d8de906b
raazadarsh <raj.ada@northeastern.edu>
2024-05-30 10:35:34 -0400

    Adarsh: Updated workflow file, and variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami_id" {
   description = "Value of the jenkins ami image"
   type        = string
-  #  default     = "ami-0672ad10ddf65a6ee"
+  # default     = "ami-0fa6dc91476746646"
 }
 
 variable "instance_type" {
@@ -22,20 +22,32 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "required_version" {
-  description = "Minimum required version of tf"
-  type        = string
-  default     = ">= 1.2.0"
-}
+# variable "required_version" {
+#   description = "Minimum required version of tf"
+#   type        = string
+#   default     = ">= 1.2.0"
+# }
 
-variable "aws_provider_version" {
-  description = "Required version of tf"
-  type        = string
-  default     = "~>4.16"
-}
+# variable "aws_provider_version" {
+#   description = "Required version of tf"
+#   type        = string
+#   default     = "~>4.16"
+# }
 
 variable "allocation_id" {
   description = "Allocated ID of EIP"
   type        = string
   default     = "eipalloc-04d1bcc0cd1f66ba3"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR range"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "Subnet CIDR range"
+  type        = string
+  default     = "10.0.1.0/24"
 }
